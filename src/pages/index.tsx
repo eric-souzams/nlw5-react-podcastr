4315f87eb8be6bd7c6fcf60f1629245c696b6ecf
Eric M.S <15831837+ericmss@users.noreply.github.com>
2021-04-22 12:03:37 -0300

    wip: implements player context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image'
 import Link from 'next/link';
+
 import ptBR from 'date-fns/locale/pt-BR';
 import { format, parseISO } from 'date-fns';
 
@@ -8,6 +9,8 @@ import { api } from '../services/api';
 import { convertDurationTimeToString } from '../utils/convertDurationTimeToString';
 
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 type Episode = {
   id: string;
@@ -26,6 +29,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage} >
       <section className={styles.latestEpisodes}>
@@ -52,7 +57,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episodio"/>
                 </button>
               </li>
